e3d991bdda7b4555c0fa33a104aa9637c27c39f7
Joy <medidiaslijoy@gmail.com>
2026-06-16 17:46:04 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blogplatform-eprb.onrender.com",
+  baseURL: "https://blogplatform-eprb.onrender.com/api",
 });
 
 export default API;
